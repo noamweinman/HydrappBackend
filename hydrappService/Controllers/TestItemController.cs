2b6888c3e27b3c5e827d72dcf4e845a2e4b65537
Noam Yosef <noamweinman@gmail.com>
2017-01-06 21:35:25 +0200

    add the basic code

---
+++ hydrappService/Controllers/TestItemController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using hydrappService.DataObjects;
+using hydrappService.Models;
+
+namespace hydrappService.Controllers
+{
+    public class TestItemController : TableController<TestItem>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            hydrappContext context = new hydrappContext();
+            DomainManager = new EntityDomainManager<TestItem>(context, Request);
+        }
+
+        // GET tables/TestItem
+        public IQueryable<TestItem> GetAllTestItem()
+        {
+            return Query(); 
+        }
+
+        // GET tables/TestItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<TestItem> GetTestItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TestItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<TestItem> PatchTestItem(string id, Delta<TestItem> patch)
+        {
+             return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TestItem
+        public async Task<IHttpActionResult> PostTestItem(TestItem item)
+        {
+            TestItem current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TestItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTestItem(string id)
+        {
+             return DeleteAsync(id);
+        }
+    }
+}
---
+++ hydrappService/DataObjects/TestItem.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+
+namespace hydrappService.DataObjects
+{
+    public class TestItem : EntityData
+    {
+        public string name { get; set; }
+
+        public DateTime date { get; set; }
+    }
+}
