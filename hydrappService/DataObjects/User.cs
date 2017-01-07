a0f354162eade017d98068124dd6a40b16a5a2a9
Noam Yosef <noamweinman@gmail.com>
2017-01-07 15:31:36 +0200

    String to string in User DTO

--- hydrappService/DataObjects/User.cs
+++ hydrappService/DataObjects/User.cs
@@ -8,9 +8,9 @@ namespace hydrappService.DataObjects
 {
     public class User: EntityData
     {
-        public String userName { get; set; }
-        public String password { get; set; }
-        public String email { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
         public double bmi { get; set; }
     }
 }
